145457d305919c593c8c4707abb3cbfeebf69077
aglegaspi <lex83nyc@gmail.com>
2019-07-14 18:51:16 -0400

    first commit

--- Hangman/Hangman/main.swift
+++ Hangman/Hangman/main.swift
@@ -8,5 +8,37 @@
 
 import Foundation
 
-print("Hello, World!")
+//func getInput() -> String? {
+//    print("get input:", terminator: " ")
+//    let lineIn = readLine() ?? nil
+//    return lineIn
+//}
+//
+//print(getInput())
 
+func gameLoop() {
+    
+    while !gameOver {
+        
+        var counter = 0
+        var gameIsNotOver = true
+        let input = getInput()
+        
+        if input == "" {
+            print("no input try again")
+            }
+        
+        continue
+        
+    } else {
+    
+        if counter > 7 {
+            gameIsNotOver = false
+            continue
+        }
+        
+        counter += 1
+    }
+    
+    print("game over")
+}
